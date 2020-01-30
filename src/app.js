61afa5adad19112198343f6d1df683e6ed749493
thehapax <39811582+thehapax@users.noreply.github.com>
2020-01-30 15:14:48 -0800

    change to environment variables

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
-const btcpay_url = 'https://btcpay139425.lndyn.com/'
-const merch_id = '5hxcRtNJgm83Kq92hjKjyXqoELyL2SpzZZf64Q1ZT5Gd'
+const btcpay_url = process.env.BTCPAY_URL
+const merch_id = process.env.BTCPAY_MERCHANT
+const store_id = process.env.STORE_ID
+const btcpay_key =  process.env.BTCPAY_KEY
 
 
 const btcpay = require('btcpay')
-const keypair = btcpay.crypto.load_keypair(new Buffer.from(process.env.BTCPAY_KEY, 'hex'))
+const keypair = btcpay.crypto.load_keypair(new Buffer.from(btcpay_key, 'hex'))
 
 // Recreate client
 const client = new btcpay.BTCPayClient(btcpay_url, keypair, {merchant: merch_id})
+
+// get btc/usd rates
+const btc_rates = client.get_rates(['BTC_USD'], store_id)
+btc_rates.then(rates => console.log(rates)).catch(err => console.log(err))
